d975197a0960c6a9a6fb985b7509e942ebfbe862
HansKindberg <hans.kindberg@gmail.com>
2021-12-28 15:10:41 +0100

    Active Directory test-fix

--- Source/Tests/Integration-tests/DirectoryServices/ActiveDirectoryTest.cs
+++ Source/Tests/Integration-tests/DirectoryServices/ActiveDirectoryTest.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Net.NetworkInformation;
 using System.Security.Claims;
 using System.Security.Principal;
+using IdentityModel;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RegionOrebroLan.DependencyInjection;
@@ -60,36 +61,16 @@ namespace IntegrationTests.DirectoryServices
 		}
 
 		[TestMethod]
-		[ExpectedException(typeof(InvalidOperationException))]
-		public void GetAttributesAsync_IfTheIdentifierKindParameterIsSamAccountName_And_IfTheNameClaimHasAnInvalidDomainPart_ShouldThrowAnInvalidOperationException()
-		{
-			var domain = Guid.NewGuid().ToString();
-			var name = $"{domain}\\abc123";
-
-			try
-			{
-				_ = this.ActiveDirectory.GetAttributesAsync(Enumerable.Empty<string>(), IdentifierKind.SamAccountName, new ClaimsPrincipal(new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, name) }))).Result;
-			}
-			catch(AggregateException aggregateException)
-			{
-				if(aggregateException.InnerExceptions.FirstOrDefault() is InvalidOperationException invalidOperationException)
-				{
-					if(string.Equals($"Could not get attributes for principal \"{name}\".", invalidOperationException.Message, StringComparison.Ordinal))
-					{
-						if(invalidOperationException.InnerException != null)
-						{
-							if(string.Equals($"The name-claim \"{name}\" has an invalid domain-part. The domain \"{domain}\" is invalid.", invalidOperationException.InnerException.Message, StringComparison.Ordinal))
-								throw invalidOperationException;
-						}
-					}
-				}
-			}
-		}
-
-		[TestMethod]
 		public void GetAttributesAsync_IfTheIdentifierKindParameterIsSamAccountName_ShouldWorkProperly()
 		{
-			var attributes = this.ActiveDirectory.GetAttributesAsync(new[] { "userPrincipalName" }, IdentifierKind.SamAccountName, new WindowsPrincipal(WindowsIdentity.GetCurrent())).Result;
+			var identityNameParts = WindowsIdentity.GetCurrent().Name.Split('\\', 2);
+			string samAccountName = null;
+			if(identityNameParts.Length == 2)
+				samAccountName = identityNameParts[1];
+
+			var principal = new ClaimsPrincipal(new ClaimsIdentity(new[] { new Claim(JwtClaimTypes.Name, samAccountName) }, "Test"));
+
+			var attributes = this.ActiveDirectory.GetAttributesAsync(new[] { "userPrincipalName" }, IdentifierKind.SamAccountName, principal).Result;
 
 			Assert.AreEqual(1, attributes.Count, "The test must be run on a domain.");
 		}
@@ -125,6 +106,41 @@ namespace IntegrationTests.DirectoryServices
 			Assert.AreEqual(2, attributes.Count, "The test must be run on a domain.");
 			Assert.AreEqual(samAccountName, attributes.ElementAt(0).Value, "The test must be run on a domain.");
 			Assert.AreEqual(userPrincipalName, attributes.ElementAt(1).Value, "The test must be run on a domain.");
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void GetAttributesAsync_IfTheIdentifierKindParameterIsWindowsAccountName_And_IfTheNameClaimHasAnInvalidDomainPart_ShouldThrowAnInvalidOperationException()
+		{
+			var domain = Guid.NewGuid().ToString();
+			var name = $"{domain}\\abc123";
+
+			try
+			{
+				_ = this.ActiveDirectory.GetAttributesAsync(Enumerable.Empty<string>(), IdentifierKind.WindowsAccountName, new ClaimsPrincipal(new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, name) }))).Result;
+			}
+			catch(AggregateException aggregateException)
+			{
+				if(aggregateException.InnerExceptions.FirstOrDefault() is InvalidOperationException invalidOperationException)
+				{
+					if(string.Equals($"Could not get attributes for principal \"{name}\".", invalidOperationException.Message, StringComparison.Ordinal))
+					{
+						if(invalidOperationException.InnerException != null)
+						{
+							if(string.Equals($"The name-claim \"{name}\" has an invalid domain-part. The domain \"{domain}\" is invalid.", invalidOperationException.InnerException.Message, StringComparison.Ordinal))
+								throw invalidOperationException;
+						}
+					}
+				}
+			}
+		}
+
+		[TestMethod]
+		public void GetAttributesAsync_IfTheIdentifierKindParameterIsWindowsAccountName_ShouldWorkProperly()
+		{
+			var attributes = this.ActiveDirectory.GetAttributesAsync(new[] { "userPrincipalName" }, IdentifierKind.WindowsAccountName, new WindowsPrincipal(WindowsIdentity.GetCurrent())).Result;
+
+			Assert.AreEqual(1, attributes.Count, "The test must be run on a domain.");
 		}
 
 		[TestMethod]
