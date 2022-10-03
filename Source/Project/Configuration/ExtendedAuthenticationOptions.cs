f2bb5d72c04a0bdd80067e57740c665365d16b65
HansKindberg <hans.kindberg@gmail.com>
2022-10-03 12:39:54 +0200

    Target-typed new

--- Source/Project/Configuration/ExtendedAuthenticationOptions.cs
+++ Source/Project/Configuration/ExtendedAuthenticationOptions.cs
@@ -10,11 +10,11 @@ namespace RegionOrebroLan.Web.Authentication.Configuration
 	{
 		#region Properties
 
-		public virtual ActiveDirectoryOptions ActiveDirectory { get; set; } = new ActiveDirectoryOptions();
+		public virtual ActiveDirectoryOptions ActiveDirectory { get; set; } = new();
 		public virtual IDictionary<string, DecoratorOptions> AuthenticationDecorators { get; } = new Dictionary<string, DecoratorOptions>(StringComparer.OrdinalIgnoreCase);
 		public virtual IDictionary<string, DecoratorOptions> AuthenticationPropertiesDecorators { get; } = new Dictionary<string, DecoratorOptions>(StringComparer.OrdinalIgnoreCase);
 		public virtual IDictionary<string, DecoratorOptions> CallbackDecorators { get; } = new Dictionary<string, DecoratorOptions>(StringComparer.OrdinalIgnoreCase);
-		public virtual NegotiateAuthenticationOptions Negotiate { get; set; } = new NegotiateAuthenticationOptions();
+		public virtual NegotiateAuthenticationOptions Negotiate { get; set; } = new();
 		public virtual IDictionary<string, SchemeRegistrationOptions> SchemeRegistrations { get; } = new Dictionary<string, SchemeRegistrationOptions>(StringComparer.OrdinalIgnoreCase);
 
 		#endregion
